385b83121e143393e8b1812e7a5997253b16b012
German Terrazas <germanterrazas@gmail.com>
2021-02-13 21:39:31 +0000

    Commit by German Terrazas (gt401@cam.ac.uk) on 27 January 2021 16:24 - SharedPreferences.kt class created for handling user registration / login in activity_user_registration.xml - This update has not been tested on the phone

--- app/src/main/java/uoc/ifm/dial/saamd/LFDApp/UserRegistration.kt
+++ app/src/main/java/uoc/ifm/dial/saamd/LFDApp/UserRegistration.kt
@@ -4,22 +4,50 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
-import uoc.ifm.dial.saamd.LFDApp.util.Constants
+import uoc.ifm.dial.saamd.LFDApp.util.SharedPreferences
 
 class UserRegistration : AppCompatActivity() {
+
+    private lateinit var username: String
+    private lateinit var consent: String
+    private lateinit var password: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_registration)
 
         val buttonConsent = findViewById<Button>(R.id.button_consent)
         val textViewConsent = findViewById<TextView>(R.id.textView4)
+        val editTextUsername = findViewById<EditText>(R.id.editTextUserName)
+        val editTextContact = findViewById<EditText>(R.id.editTextContact)
+
         textViewConsent.setMovementMethod(LinkMovementMethod.getInstance())
 
+        val sharedPreference:SharedPreferences = SharedPreferences(this)
+        username = sharedPreference.getValueString("USERNAME")
+        consent = sharedPreference.getValueString("CONSENT_YES")
+        password = sharedPreference.getValueString("PASSWORD")
+
         buttonConsent.setOnClickListener {
-            val startIntent = Intent(this, UserInstructions::class.java)
-            startActivity(startIntent)
+            if(username != null && consent != null && password != null){
+                Log.d("username if", username)
+                Log.d("password if", password)
+                Log.d("consent if", consent)
+            } else {
+                sharedPreference.save("USERNAME", username)
+                sharedPreference.save("CONSENT_YES", "YES")
+                sharedPreference.save("PASSWORD", password)
+                Log.d("username else", username)
+                Log.d("password else", password)
+                Log.d("consent else", consent)
+                val startIntent = Intent(this, UserInstructions::class.java)
+                startActivity(startIntent)
+            }
+
         }
     }
 }
