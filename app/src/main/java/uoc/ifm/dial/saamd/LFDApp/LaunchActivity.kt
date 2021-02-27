0e6bc55797930dd0de929335a8543adea56cb85e
German Terrazas <germanterrazas@gmail.com>
2021-02-27 00:19:46 +0000

    Commit by German Terrazas (gt401@cam.ac.uk) on 26 February 2021

--- app/src/main/java/uoc/ifm/dial/saamd/LFDApp/LaunchActivity.kt
+++ app/src/main/java/uoc/ifm/dial/saamd/LFDApp/LaunchActivity.kt
@@ -1,15 +1,19 @@
 package uoc.ifm.dial.saamd.LFDApp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import uoc.ifm.dial.saamd.LFDApp.util.Constants
 import uoc.ifm.dial.saamd.LFDApp.util.SharedPreferences
 
 class LaunchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        try {
+            this.supportActionBar!!.hide()
+        } catch (e: NullPointerException) {
+        }
         setContentView(R.layout.activity_launch)
 
         val sharedPreference = SharedPreferences(this)
